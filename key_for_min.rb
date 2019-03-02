fa11d4771cbbda2bffa8506c8cf519763d5d4353
Robert Tate <btate712@gmail.com>
2019-03-02 21:12:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,6 +12,7 @@ def key_for_min_value(name_hash)
       if value < smallest_value
         smallest_value = value
         smallest_key = key
+        puts "New Smallest Value is #{smallest_value} with a key of #{smallest_key}."
       end
     end
   end
